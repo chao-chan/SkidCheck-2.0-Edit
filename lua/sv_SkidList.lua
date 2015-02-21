6cea309c6539e65d20458726ff29221c4cddad18
HeX <MFSiNC@users.noreply.github.com>
2015-02-21 16:24:00 +0000

    ~350 more!

--- lua/sv_SkidList.lua
+++ lua/sv_SkidList.lua
@@ -12,6 +12,10 @@ Skid.HAC_DB = {
 
 
 
+	["STEAM_0:1:29289283"] = "IHATETYLER/Troll names, 3x VAC bans",
+	["STEAM_0:1:53978959"] = "Troll names",
+	["STEAM_0:1:91037148"] = "youknowtheusual, 2x VAC bans, aimware cheat",
+	["STEAM_0:1:107252732"] = "d3x alt, history of cheating, ban me",
 	["STEAM_0:1:68209808"] = "DeeJay, VAC ban, autobanned for hacks, google his steamid",
 	["STEAM_0:1:21056185"] = "ds22/DarkShadow6, 'my hax I troll kids on darkrp with get detected pretty fast'",
 	["STEAM_0:1:49225816"] = "Mew, server addon backdoor, ban me forever",
@@ -1304,7 +1308,7 @@ Skid.HAC_DB = {
 	["STEAM_0:0:26285276"] = "Banlist",
 	["STEAM_0:0:37790285"] = "Banlist",
 	["STEAM_0:0:37919749"] = "Skiddie",
-	["STEAM_0:0:33099483"] = "polivilas (hera)",
+	["STEAM_0:0:33099483"] = "polivilas/troll names, hera, VAC banned",
 	["STEAM_0:0:68140512"] = "TerriGod, ban me",
 	["STEAM_0:1:21996971"] = "Nox: Speedhacking",
 	["STEAM_0:0:42185993"] = "Skiddie",
@@ -4097,7 +4101,7 @@ Skid.HAC_DB = {
 	["STEAM_0:0:8345987"] = "SP2: Unknown",
 	["STEAM_0:0:18947080"] = "Banlist",
 	["STEAM_0:0:10713231"] = "SP2: Code Injection",
-	["STEAM_0:1:73426095"] = "Troll names, 2S: BlueBot, BAN ME FOREVER",
+	["STEAM_0:1:73426095"] = "Kami/Troll names, 4x VAC bans, 2S: BlueBot, BAN ME FOREVER",
 	["STEAM_0:0:17711590"] = "Mr Eazy",
 	["STEAM_0:0:36250162"] = "Blackops.cfg",
 	["STEAM_0:1:50887902"] = "Banlist",
